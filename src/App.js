73e11eb0c5f172c83d01ec97af8ddc7e987a291a
audreybichelmeir <bichelmeir@chapman.edu>
2021-05-21 02:07:45 +0800

    popup-form

--- src/App.js
+++ src/App.js
@@ -1,25 +1,76 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from "react"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+    constructor() {
+        super()
+        this.state = {
+            firstName: "",
+            lastName: "",
+            phoneNumber: "",
+            emailAddress: ""
+        }
+        this.handleChange = this.handleChange.bind(this)
+    }
+
+    handleChange(event) {
+        const {name, value, type, checked} = event.target
+        type === "checkbox" ?
+            this.setState({
+                [name]: checked
+            })
+        :
+        this.setState({
+            [name]: value
+        })
+    }
+
+    render() {
+        return (
+            <main>
+                <form>
+                    <input
+                        name="firstName"
+                        value={this.state.firstName}
+                        onChange={this.handleChange}
+                        placeholder="First Name"
+                    />
+                    <br />
+
+                    <input
+                        name="lastName"
+                        value={this.state.lastName}
+                        onChange={this.handleChange}
+                        placeholder="Last Name"
+                    />
+                    <br />
+
+                    <input
+                        name="phoneNumber"
+                        value={this.state.phoneNumber}
+                        onChange={this.handleChange}
+                        placeholder="Phone Number"
+                    />
+                    <br />
+
+                    <input
+                        name="emailAddress"
+                        value={this.state.emailAddress}
+                        onChange={this.handleChange}
+                        placeholder="Email Address"
+                    />
+                    <br />
+
+
+                    <button>Submit</button>
+                </form>
+                <hr />
+                <h2>Entered information:</h2>
+                <p>Your name: {this.state.firstName} {this.state.lastName}</p>
+                <p>Your phone number: {this.state.phoneNumber}</p>
+                <p>Your email address: {this.state.emailAddress}</p>
+            </main>
+        )
+    }
 }
 
-export default App;
+export default App
